e3c0359de5a0e0dc561a4ea6725f4bde0130c742
dvkrrrr <dvkrrrr@gmail.com>
2020-01-24 21:32:49 +0300

    cool map

--- Main.py
+++ Main.py
@@ -3,12 +3,10 @@ import sys
 
 import pygame
 
-
-FPS = 50
-
+FPS = 40
 
 pygame.init()
-size = WIDTH, HEIGHT = 700, 700
+size = WIDTH, HEIGHT = 1024, 576
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 GRAVITY = 5
@@ -33,23 +31,8 @@ def terminate():
 
 
 def start_screen():
-    intro_text = ["Приветствую вас", "",
-                  "Для начала игры нажмите",
-                  "ENTER"]
-
-    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    fon = pygame.transform.scale(load_image('loading....png'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 50
-    for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('black'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
-
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -59,6 +42,11 @@ def start_screen():
                     return
         pygame.display.flip()
         clock.tick(FPS)
+
+
+def choose_level():
+    fon = pygame.transform.scale(load_image('ds.png'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
 
 
 def load_level(filename):
@@ -71,10 +59,15 @@ def load_level(filename):
     return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
 
-tile_images = {'wall': load_image('fon.jpeg'), 'empty': load_image('grass.png')}
-player_image = load_image('trump.png')
+tile_images = {'grow': load_image('grow.png'), 'grass': load_image('grass.jpg'),
+               'pech': load_image('pech.jpg'), 'verstac': load_image('verstac.jpg'),
+               'stone': load_image('stone.jpg'), 'tree': load_image('tree.png')}
+player_image = [load_image('trump.png'), load_image('trump_run (1).png'), load_image('trump_run (2).png'),
+                load_image('trump_run (4).png'), load_image('trump_run (5).png'), load_image('trump_run (6).png'),
+                load_image('trump_run (7).png'), load_image('trump_run (8).png'), load_image('trump_run (9).png'),
+                load_image('trump_run (11).png'), load_image('trump_run (13).png'), load_image('trump_run (15).png')]
 
-tile_width = tile_height = 50
+tile_width = tile_height = 39
 
 
 class Tile(pygame.sprite.Sprite):
@@ -87,10 +80,25 @@ class Tile(pygame.sprite.Sprite):
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__(player_group, all_sprites)
-        self.image = player_image
+        self.frames = player_image
+        self.image = self.frames[0]
+        self.cur_frame_right = 0
+        self.cur_frame_left = 0
         self.pos_x = tile_width * pos_x + 15
         self.pos_y = tile_height * pos_y + 5
         self.rect = self.image.get_rect().move(tile_width * pos_x + 15, tile_height * pos_y + 5)
+
+    def update_right(self):
+        self.cur_frame_right = 1 + ((self.cur_frame_right + 1) % 5)
+        self.image = self.frames[self.cur_frame_right]
+
+    def update_left(self):
+        self.cur_frame_left = 7 \
+                              + ((self.cur_frame_left + 1) % 5)
+        self.image = self.frames[self.cur_frame_left]
+
+    def update(self):
+        self.image = self.frames[0]
 
 
 player = None
@@ -104,13 +112,18 @@ def generate_level(level):
     new_player, x, y = None, None, None
     for y in range(len(level)):
         for x in range(len(level[y])):
-            # if level[y][x] == '.':
-            #     Tile('empty', x, y)
-            if level[y][x] == '#':
-                Tile('wall', x, y)
+            if level[y][x] == '%':
+                Tile('grass', x, y)
             elif level[y][x] == '@':
-                # Tile('empty', x, y)
                 new_player = Player(x, y)
+            elif level[y][x] == '*':
+                Tile('tree', x, y)
+            elif level[y][x] == '{':
+                Tile('stone', x, y)
+            elif level[y][x] == '#':
+                Tile('grow', x, y)
+            elif level[y][x] == '/':
+                Tile('tree', x, y)
     return new_player, x, y
 
 
@@ -128,6 +141,10 @@ class Camera:
         self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
 
 
+#pygame.mixer.music.load('first.mp3')
+#pygame.mixer.music.play()
+
+
 def play():
     x = 0
     y = 0
@@ -138,40 +155,53 @@ def play():
             if event.type == pygame.QUIT:
                 terminate()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP or event.key == \
+                if event.key == pygame.K_SPACE or event.key == \
                         pygame.K_DOWN or event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
                     q = event.key
                     f = True
             if event.type == pygame.KEYUP:
-                if event.key == pygame.K_UP or event.key == \
+                if event.key == pygame.K_SPACE or event.key == \
                         pygame.K_DOWN or event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
                     q = event.key
                     f = False
         if f:
-            if q == pygame.K_UP:
-                y = 10
-            if q == pygame.K_RIGHT:
+            if q == pygame.K_SPACE and y == 0 and pygame.sprite.spritecollideany(player, tiles_group):
+                y = 25
+            player.rect.y -= 5
+            player_group.draw(screen)
+            if q == pygame.K_RIGHT and not pygame.sprite.spritecollideany(player, tiles_group):
                 x += 5
-            if q == pygame.K_LEFT:
+            if q == pygame.K_LEFT and not pygame.sprite.spritecollideany(player, tiles_group):
                 x = -5
+            player.rect.y += 5
+            player_group.draw(screen)
         if not pygame.sprite.spritecollideany(player, tiles_group):
-            y -= GRAVITY
-
+            player.rect.y += GRAVITY
         screen.fill((0, 0, 0))
-        all_sprites.draw(screen)
         player.rect.x += x
         player.rect.y -= y
-        x, y = 0, 0
+        if x > 0:
+            player.update_right()
+        if x < 0:
+            player.update_left()
+        if x == 0:
+            player.update()
+
+        y -= 1
+        if y < 20:
+            y = 0
+        x = 0
         camera.update(player)
         for sprite in all_sprites:
             camera.apply(sprite)
         player_group.draw(screen)
+        all_sprites.draw(screen)
         pygame.display.flip()
         clock.tick(FPS)
 
 
 player, level_x, level_y = generate_level(load_level('map.txt'))
 
-
 start_screen()
+choose_level()
 play()
